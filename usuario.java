fdb9b1b7cce41ae39c9ea1f4799fe377022e2817
Gustavo Santos <gliraeditions@gmail.com>
2017-04-13 19:36:43 -0300

    Update usuario.java

--- usuario.java
+++ usuario.java
@@ -1,81 +1,80 @@
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.Driver;
-import com.mysql.jdbc.Statement;
 
 public class usuario {
-	String area;
-	String email;
-	int id;
-	String nome;
-	String pass;
-	int telefone;
+	private int id;
+    private String nome;
+    private String email;
+    private String senha;
+    private boolean vip;
+    private int telefone;
+    private String area;
 	
-	//CONECTANDO AO BD
-	public void Abrirb() throws SQLException, ClassNotFoundException{
-	Driver meuDriver = new com.mysql.jdbc.Driver();
-	DriverManager.registerDriver(meuDriver);
-	String usuario = "root";
-	String senha = "";
-	String url = "jdbc:mysql://localhost/bdadx";
-	try{
-		System.out.println("Abrindo conexão com o banco de dados");
-		Connection conn = (Connection) DriverManager.getConnection(url, usuario, senha);
-		System.out.println("Conectado");
-		conn.close();
-	} catch(SQLException erro){
-		System.out.println("Erro de conexão com o banco2");
-			
-		}
-}
-	//CONECTADO
-	
-	public String getArea() {
-		Statement stmt = (Statement) conn.createStatement();
-		String sql = "SELECT nome, cnpj, telefone FROM tb_empresa";
-		ResultSet resultado = stmt.executeQuery(sql);
-		while(resultado.next()){
-			String nome = resultado.getString("nome");
-			String cnpj = resultado.getString("cnpj");
-			int telefone = resultado.getInt("telefone");
-			System.out.println("Nome:"+nome+", CNPJ:"+cnpj+", Telefone:"+telefone);
-		}
-	}
-	public void setArea(String area) {
-		this.area = area;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getPass() {
-		return pass;
-	}
-	public void setPass(String pass) {
-		this.pass = pass;
-	}
-	public int getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(int telefone) {
-		this.telefone = telefone;
-	}
-	
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isVip() {
+        return vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+    
+    public usuario(){}
+    
+    public usuario(int id, String nome, String email, String senha, boolean vip, int telefone, String area){
+        this.id = id; 
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.vip = vip;
+        this.telefone = telefone;
+        this.area = area;
+    }
 }
